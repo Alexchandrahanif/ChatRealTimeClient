15bf3fb3574be5e83dc706499a1dbca7cd97dc75
Alex Chandra Hanif <alexchandra1998@gmail.com>
2024-01-18 10:05:35 +0700

    update menambahkan fitur full screen

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
 const ProfilePage = () => {
+  const toggleFullscreen = () => {
+    const element = document.documentElement
+
+    if (!document.fullscreenElement) {
+      element.requestFullscreen().catch((err) => {
+        console.error(`Error attempting to enable fullscreen: ${err.message}`)
+      })
+    } else {
+      document.exitFullscreen()
+    }
+  }
+
   return (
-    <div>ProfilePage</div>
+    <div className="container mx-auto my-4 text-center">
+      <button
+        className="bg-blue-500 text-white py-2 px-4 rounded"
+        onClick={toggleFullscreen}
+      >
+        Toggle Fullscreen
+      </button>
+      {/* Konten aplikasi lainnya */}
+    </div>
   )
 }
 
